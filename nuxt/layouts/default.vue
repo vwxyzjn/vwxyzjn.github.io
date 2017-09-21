03604a57f97ff11798285814c08468e206837a13
Costa Huang <costa.huang@outlook.com>
2017-09-20 20:18:12 -0400

    Add page contents

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,52 +1,114 @@
 <template>
-  <div>
-    <nuxt/>
+  <div id="app">
+    <div class="container-fluid" >
+      <div class="row">
+        <div class="sidebar col-md-4">
+          <div class="profile_info">
+            <img class="profile_picture" src="~/assets/profile_picture.svg">
+            <h1>Costa <span style="font-weight:bold">Huang</span></h1>
+            <h5>strive for interesting things</h5>
+            <div class="menu-item-wrapper">
+              <router-link to="/">resume</router-link>
+              <router-link to="/research">research</router-link>
+              <a href="https://drive.google.com/open?id=0B0wkgJhWMQfgazItYXdsWk9ZUkU">math</a>
+              <a href="https://github.com/vwxyzjn">github</a>
+            </div>
+          </div>
+        </div>
+        <div class="content col-md-6">
+          <transition name="fade" mode="out-in">
+            <nuxt />
+          </transition>
+          <!-- <resume></resume> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+import '../node_modules/bootstrap/scss/bootstrap.scss'
+export default {
+  name: 'app',
+  methods: {
+    to_resume () {
+      this.$router.push('/')
+    },
+    to_research () {
+      this.$router.push('/research')
+    }
+  }
 }
+</script>
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+<style lang="sass" scoped>
+*
+  font-family: 'Roboto', sans-serif, -apple-system, "Helvetica Neue", Arial
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+.fade-enter-active, .fade-leave-active
+  transition: opacity .35s
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+.fade-enter, .fade-leave-to
+  opacity: 0
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+.sidebar
+  position: relative
+  bottom: 0
+  top: 0
+  padding-top: 5vh
+  text-align: center
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  img
+    width: 130px
+    height: 130px
+
+  h5
+    font-size: 1.05em
+    color: #53657D
+
+  .menu-item-wrapper
+    display: flex
+    width: 100%
+    justify-content: center
+    flex-wrap: wrap
+    padding-top: 18px
+
+    a
+      padding: 10px
+      color: #9DBF15
+
+    .router-link-exact-active
+      color: black
+
+.content
+  padding: 40px
+  position: relative
+  bottom: 0
+
+@media (min-width: 768px)
+  .sidebar
+    height: 100vh
+    padding-top: 20vh
+    position: sticky
+    text-align: right
+
+    .profile_info
+      display: inline-block
+      text-align: center
+      width: 70%
+
+  #app
+    padding-right: calc(100% - 100vw + 17px)
+
+
+  .content
+    padding-top: 60px
+
+
+.title
+  margin-bottom: 20px
+
+.section
+  padding-bottom: 30px
+
 </style>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,58 +1,109 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+.resume
+  .education.section
+    h2.title Education
+    hr
+    .item
+      #textbox
+        h6.position_title Furman University, Greenville, SC
+        h6.related_time May 2018
+      ul
+        li Bachelor of Science in Computer Science
+        li Bachelor of Science in Applied Math
+
+  .experience.section
+    h2.title Experience
+    hr
+    // Math & Computer Science Tutor
+    .item
+      #textbox
+        h6.position_title Computer Science Lab Assistant
+        h6.related_time Spring 2017—Present
+      h6.address Furman University Computer Science Department, Greenville, SC
+      ul
+        li Tutored Python in terms of picture manipulation, sound editing and debugging
+        li Provide up to 10 hours of tutoring every week
+    // Vice President of Technology
+    .item
+      #textbox
+        h6.position_title Vice President of Technology
+        h6.related_time Fall 2016—Present
+      h6.address Furman University Investment Club, Greenville, SC
+      ul
+        li Update portfolio information whenever new trade is made
+        li
+          | Founded 
+          a(href='http://portfolio.fuinvestment.com') Fuinvestment.com 
+          |  that shows equity curve, portfolio, and transactions history
+        li
+          | Collaborated with other core members to raise campus awareness and introduced stock tracker game
+    // Math & Computer Science Tutor
+    .item
+      #textbox
+        h6.position_title Math & Computer Science Tutor
+        h6.related_time Fall 2016—Present
+      h6.address Furman University Tutoring Center, Greenville, SC
+      br
+    // Accounting Intern
+    .item
+      #textbox
+        h6.position_title Accounting Intern
+        h6.related_time Summer 2015
+      h6.address DBO LLC, Hainan Division, China
+      br
+    // Tax Intern
+    .item
+      #textbox
+        h6.position_title Tax Intern
+        h6.related_time Spring 2015
+      h6.address Volunteer Income Tax Assistance (VITA), , Greenville, SC
+      br
+
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
+  name: 'resume'
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+#textbox {
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.section {
+  padding-bottom: 25px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.position_title{
+  font-style: italic; 
 }
 
-.links {
-  padding-top: 15px;
+.related_time {
+  font-style: italic; 
+}
+
+.address {
+  opacity: 0.7;
+  color: #53657D;
+}
+
+.experience {
+  .item {
+    padding-bottom: 13px;
+  }
+}
+
+li {
+  font-size: 14px;
+}
+
+@media (min-width: 768px) {
+  #textbox {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 }
 </style>
