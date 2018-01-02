f0103d97b52a20b9af5ffa8c316d6b9bf57bf8b8
Unknown <vwxyzjn@foxmail.com>
2018-01-02 18:26:45 +0800

    Add more descriptions in the portfolio page

--- components/Profile.vue
+++ components/Profile.vue
@@ -9,7 +9,7 @@
     </b-col>
     <b-col cols="12" class="text-center pt-2">
       <b-link to="/" exact-active-class="exact-active-class">home</b-link>
-      <b-link to="/research" exact-active-class="exact-active-class">research</b-link>
+      <b-link to="/portfolio" exact-active-class="exact-active-class">portfolio</b-link>
       <b-link href="https://costahuang.me/resume.pdf">resume</b-link>
       <b-link href="https://github.com/vwxyzjn" >github</b-link>
     </b-col>
@@ -19,6 +19,9 @@
 <style lang="sass" scoped>
 .exact-active-class
   color: black
+
+h1 
+  margin-bottom: 0.3em
 
 a
   color: #9DBF15
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,15 +1,29 @@
 <template lang="pug">
   div
-    h1 Projects
     b-card.mb-3(title='Reproduction of Deepmind’s StarCraft II Research', tag='article')
+      p.card-text 
+      | I integrated Tensorforce (a Reinforcement Learning library on top of 
+      | Tensorflow) and OpenAI Gym to train SC II game agents, resulting in
+      | maintainable and understandable machine learning code.
       b-link.card-link(href='https://costahuang.me/SC2AI') Jupyter Writeup
       b-link.card-link(href='https://github.com/vwxyzjn/SC2AI') Github
   
     b-card.mb-3(title='Linear Programming for Optimal Scheduling', tag='article')
+      p.card-text 
+      | For our Operations Research case study about a shceduling problem, 
+      | I modeled it by using integer programming and later solved it by using
+      | Gurobipy, a python optimiazation pacakge.
       b-link.card-link(href='http://costahuang.me/files/Linear_Programming_for_Optimal_Scheduling.pdf') PDF
+      b-link.card-link(href='http://costahuang.me/LP_optimization_python/') Jupyter Writeup
       b-link.card-link(href='https://github.com/vwxyzjn/LP_optimization_python') Github
 
     b-card.mb-3(title='Sentiment Analysis of Movie Review', tag='article')
-      b-link.card-link(href='http://costahuang.me/files/Linear_Programming_for_Optimal_Scheduling.pdf') Jupyter Writeup
-      b-link.card-link(href='https://github.com/vwxyzjn/LP_optimization_python') Github
+      p.card-text Used neural network to classify movie reviews based on sentiment.
+      b-link.card-link(href='https://costahuang.me/Sentiment-Analysis-LSTM/') Jupyter Writeup
+      b-link.card-link(href='https://github.com/vwxyzjn/Sentiment-Analysis-LSTM') Github
 </template>
+
+<style lang="sass" scoped>
+  h4
+    font-weight: bold
+</style>
