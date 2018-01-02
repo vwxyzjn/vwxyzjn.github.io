187d8c62c27a74c3ada57fc5f1ef909d8cffa596
Unknown <vwxyzjn@foxmail.com>
2018-01-02 17:50:34 +0800

    add portofolio page

---
+++ pages/portfolio.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  div
+    h1 Projects
+    b-card.mb-3(title='Reproduction of Deepmind’s StarCraft II Research', tag='article')
+      b-link.card-link(href='https://costahuang.me/SC2AI') Jupyter Writeup
+      b-link.card-link(href='https://github.com/vwxyzjn/SC2AI') Github
+  
+    b-card.mb-3(title='Linear Programming for Optimal Scheduling', tag='article')
+      b-link.card-link(href='http://costahuang.me/files/Linear_Programming_for_Optimal_Scheduling.pdf') PDF
+      b-link.card-link(href='https://github.com/vwxyzjn/LP_optimization_python') Github
+
+    b-card.mb-3(title='Sentiment Analysis of Movie Review', tag='article')
+      b-link.card-link(href='http://costahuang.me/files/Linear_Programming_for_Optimal_Scheduling.pdf') Jupyter Writeup
+      b-link.card-link(href='https://github.com/vwxyzjn/LP_optimization_python') Github
+</template>
