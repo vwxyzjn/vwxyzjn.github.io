9dc5c86271f3bafc8d60d17996ec844f3c1adef6
Costa Huang <costa.huang@outlook.com>
2018-01-22 09:41:19 -0500

    Use png instead of svg

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row align-h="center">
     <b-col cols="12" class="text-center">
-      <img class="profile_picture" src="~/assets/profile_picture.svg" width="130" height="130">
+      <img class="profile_picture" src="~/assets/profile_picture.png" width="130" height="130">
     </b-col>
     <b-col cols="12" class="text-center">
       <h1>Costa <span style="font-weight:bold">Huang</span></h1>
@@ -10,7 +10,7 @@
     <b-col cols="12" class="text-center pt-2">
       <b-link to="/" exact-active-class="exact-active-class">home</b-link>
       <b-link to="/portfolio" exact-active-class="exact-active-class">portfolio</b-link>
-      <b-link href="https://costahuang.me/CostaHuang.pdf">resume</b-link>
+      <b-link href="https://costahuang.me/Costa-Huang-Resume.pdf">resume</b-link>
       <b-link href="https://github.com/vwxyzjn" >github</b-link>
     </b-col>
   </b-row>
